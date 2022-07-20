82845f110f9e4317de86b19e0d12369bd94f4b6b
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-07-20 09:44:54 -0300

    Add files via upload

---
+++ springsecurityexample/src/main/java/com/standart/springsecurityexample/domain/TestModel/models/TestModel.java
@@ -0,0 +1,38 @@
+package com.standart.springsecurityexample.domain.TestModel.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "Models")
+public class TestModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "available")
+    private Boolean isAvailable;
+
+}
