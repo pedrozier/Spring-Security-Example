9aece5028764fcd6a039074fbb5d3ce70a296123
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-07-25 11:31:55 -0300

    Add files via upload

---
+++ springsecurityexample/src/main/java/com/standard/springsecurityexample/domain/TestModel/models/TestModel.java
@@ -0,0 +1,37 @@
+package com.standard.springsecurityexample.domain.TestModel.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "MODELS_TEST")
+public class TestModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "available")
+    private Boolean isAvailable;
+
+}
---
+++ springsecurityexample/src/main/java/com/standard/springsecurityexample/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.standard.springsecurityexample.enums;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER;
+}
