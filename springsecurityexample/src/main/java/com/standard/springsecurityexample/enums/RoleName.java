1185328dc6593701e3b583a9b336e22eb726e5f0
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-07-25 11:31:05 -0300

    Delete springsecurityexample directory

--- springsecurityexample/src/main/java/com/standard/springsecurityexample/enums/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.standard.springsecurityexample.enums;
-
-public enum RoleName {
-    ROLE_ADMIN,
-    ROLE_USER;
-}
--- springsecurityexample/src/main/java/com/standart/springsecurityexample/enums/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.standart.springsecurityexample.enums;
-
-public enum RoleName {
-    ROLE_ADMIN,
-    ROLE_USER;
-}
